1a4572fd658f8b68e8bb0024bd78227fe8a1d73b
Jess <jess.harrelson0707@gmail.com>
2016-11-07 10:13:40 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 'use strict';
+
+function Dog(name, breed, age){
+  this.name = name;
+  this.breed = breed;
+  this.age = age;
+}
+
+function Cat(name, breed, age){
+  this.name = name;
+  this.breed = breed;
+  this.age = age;
+}
+
+function Ferret(name, age, foodRequirements){
+  this.name = name;
+  this.age = age;
+  this.foodRequirements = foodRequirements;
+}
+
+function Bird(name, breed){
+  this.name = name;
+  this.breed = breed;
+}
+
+function Fish(name, species, waterConditions){
+  this.name = name;
+  this.species = species;
+  this.waterConditions = waterConditions;
+}
+
+
+// function Sandwich(bread, crust, meat, condiments, veggies, cheese) {
+//   this.breadType = bread;
+//   this.crust = crust;
+//   this.meat = meat;
+//   this.condiments = condiments;
+//   this.veggies = veggies;
+//   this.cheese = cheese;
+// }
